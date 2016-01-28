98792a2ca1ecbf85ba7b0f4f48204327a3b00af6
Ju Liu <ju.liu@welaika.com>
2016-01-28 11:37:14 +0000

    Extract methods in parser logic

--- addon/parser.js
+++ addon/parser.js
@@ -10,35 +10,62 @@ export default {
       rootField.selectionSet.push(field);
     });
 
-    model.eachRelationship((relName, {kind, type, options}) => {
+    model.eachRelationship((relName, relationship) => {
+      let field;
+      let {type, options} = relationship;
+
       if (options.async) {
-        let suffix = kind === 'hasMany' ? 'Ids' : 'Id';
-        let field = new Type.Field(Ember.String.singularize(relName) + suffix);
-        rootField.selectionSet.push(field);
+        field = this._buildAsyncRelationship(relName, relationship);
       } else {
         let relModel = store.modelFor(type);
-        let camelizedType = Ember.String.camelize(type);
-        let modelName = kind === 'hasMany' ? Ember.String.pluralize(camelizedType) : camelizedType;
-        let alias = modelName !== relName && relName;
-
-        let field = new Type.Field(
-          modelName,
-          alias,
-          new Type.ArgumentSet(),
-          new Type.SelectionSet(new Type.Field('id'))
-        );
-
-        relModel.eachAttribute(function(attr) {
-          let relField = new Type.Field(attr);
-          field.selectionSet.push(relField);
-        });
-
-        rootField.selectionSet.push(field);
+        field = this._buildSyncRelationship(relModel, relName, relationship);
       }
+
+      rootField.selectionSet.push(field);
     });
 
     operation.selectionSet.push(rootField);
 
     return operation;
+  },
+
+  _buildAsyncRelationship(relName, {kind}) {
+    let suffix = kind === 'hasMany' ? 'Ids' : 'Id';
+    return new Type.Field(Ember.String.singularize(relName) + suffix);
+  },
+
+  _buildSyncRelationship(relModel, relName, {kind, type}) {
+    let normalizedType = this._getNormalizedType(kind, type);
+    let aliasedNameOrNull = this._getAliasedName(relName, normalizedType);
+
+    let field = new Type.Field(
+      normalizedType,
+      aliasedNameOrNull,
+      new Type.ArgumentSet(),
+      new Type.SelectionSet(new Type.Field('id'))
+    );
+
+    relModel.eachAttribute(function(attr) {
+      let relField = new Type.Field(attr);
+      field.selectionSet.push(relField);
+    });
+
+    return field;
+  },
+
+  _getNormalizedType(kind, type) {
+    let camelizedType = Ember.String.camelize(type);
+
+    if (kind === 'hasMany') {
+      return Ember.String.pluralize(camelizedType);
+    } else {
+      return camelizedType;
+    }
+  },
+
+  _getAliasedName(relName, normalizedType) {
+    if (relName !== normalizedType) {
+      return relName;
+    }
   }
 };
