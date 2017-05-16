90cd012814948d573988e0a9083cb5e5ef1b87aa
Kaosisochukwu Uzokwe <delkopiso@users.noreply.github.com>
2017-05-16 17:37:22 -0400

    Stop extending Array (#128)
    Instead of extending the native class Array, provide a `toArray()` method which returns a real instance of an Array for the caller to do as they see fit.

--- tests/unit/types/selection-set-test.js
+++ tests/unit/types/selection-set-test.js
@@ -8,21 +8,21 @@ test("is stack-like", function(assert) {
 
   set.push(3);
 
-  assert.equal(set[0], 1);
-  assert.equal(set[1], 2);
-  assert.equal(set[2], 3);
+  assert.equal(set.get(0), 1);
+  assert.equal(set.get(1), 2);
+  assert.equal(set.get(2), 3);
 
   let popped = set.pop();
 
   assert.equal(popped, 3);
-  assert.equal(set[2], null);
+  assert.equal(set.get(2), null);
 });
 
 test("it can be iterated over", function(assert) {
   let set = new SelectionSet(1, 2);
   let acc = [];
 
-  set.forEach(function(el){
+  set.toArray().forEach(function(el){
     acc.push(el);
   });
 
